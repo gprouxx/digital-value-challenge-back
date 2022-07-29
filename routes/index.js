47261ad13f34a9af5f1fd4adaf69db42fed32d19
Guillaume PROUX-DELROUYRE <g.prouxdelrouyre-ext@agrial.com>
2022-07-29 15:51:15 +0200

    feat: level 2

--- routes/index.js
+++ routes/index.js
@@ -10,19 +10,34 @@ const db = new sqlite3.Database("db.sqlite", err => {
     console.log("Connexion réussie à la base de données 'db.sqlite'");
 });
 
-const GET_ALL_CATEGORIES = "select id, name from categories";
+const GET_ALL_CATEGORIES = "select * from categories";
 
 /* Instructions:
  - run "npm start"
  - call localhost:3000/web/v1/categories
 */
-router.get('/web/v1/categories', function(req, res, next) {
+router.get('/web/v1/categories', function (req, res, next) {
     db.all(GET_ALL_CATEGORIES, [], (err, categories) => {
         if (err) {
             return console.log(err.message);
         }
 
-        res.send(categories)
+        let results = []
+        for (const category of categories) {
+            let result = {}
+            result.id = category.id
+            result.name = category.name
+            let children = []
+            for (const child of categories) {
+                if (child.parent_id === category.id) {
+                    children.push({id: child.id, name: child.name})
+                }
+            }
+            result.children = JSON.parse(JSON.stringify(children))
+            results.push(result)
+        }
+
+        res.send(results)
     });
 });
 
